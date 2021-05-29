92c9d501a6d300d46ed07dabd76c3ba7ad29f49e
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-05-29 22:45:01 +0900

    お気に入りページのリストビュー作成

--- lib/screens/favorite.dart
+++ lib/screens/favorite.dart
@@ -1,3 +1,4 @@
+import 'package:bicycle/data/favorite_data.dart';
 import 'package:flutter/material.dart';
 
 class Favorite extends StatefulWidget {
@@ -6,10 +7,45 @@ class Favorite extends StatefulWidget {
 }
 
 class _FavoriteState extends State<Favorite> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: ListView.builder(
+        itemCount: favorites.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            margin: EdgeInsets.all(6),
+            child: Padding(
+              padding: const EdgeInsets.all(6),
+              child: ListTile(
+                title: Text(favorites[index]['name']),
+                trailing: Wrap(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.location_on,color: Colors.redAccent,),
+                      onPressed: _jumpToSpot,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: _deleteFavorite,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+      ),
     );
+  }
+
+  void _jumpToSpot() {
+    print('スポットへ移動！');
+  }
+
+  void _deleteFavorite() {
+    print('このお気に入りを削除しますか？');
   }
 }
