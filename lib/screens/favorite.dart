7c7405d3b0dd586892712b5a3a86aae0f1cf84cd
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-05-30 01:44:43 +0900

    Consumer追加

--- lib/screens/favorite.dart
+++ lib/screens/favorite.dart
@@ -1,5 +1,7 @@
 import 'package:bicycle/data/favorite_data.dart';
+import 'package:bicycle/provider/favorite_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Favorite extends StatefulWidget {
   @override
@@ -7,36 +9,44 @@ class Favorite extends StatefulWidget {
 }
 
 class _FavoriteState extends State<Favorite> {
-
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8),
-      child: ListView.builder(
-        itemCount: favorites.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            margin: EdgeInsets.all(6),
-            child: Padding(
-              padding: const EdgeInsets.all(6),
-              child: ListTile(
-                title: Text(favorites[index]['name']),
-                trailing: Wrap(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.location_on,color: Colors.redAccent,),
-                      splashRadius: 24,
-                      onPressed: _jumpToSpot,
+      child: Consumer<FavoriteProvider>(
+        builder: (context, model, child) {
+          final favoriteList = model.favoriteList;
+          return ListView.builder(
+            itemCount: favoriteList.length,
+            itemBuilder: (BuildContext context, int index) {
+              final favorite = favoriteList[index];
+              return Card(
+                margin: EdgeInsets.all(6),
+                child: Padding(
+                  padding: const EdgeInsets.all(6),
+                  child: ListTile(
+                    title: Text(favorite.spotName),
+                    trailing: Wrap(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.location_on, color: Colors.redAccent,),
+                          splashRadius: 24,
+                          onPressed: _jumpToSpot,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          splashRadius: 24,
+                          onPressed: () {
+                            // TODO: 削除確認ダイアログ出す
+                            model.deleteFavoriteSpot(favorite.id);
+                          },
+                        ),
+                      ],
                     ),
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      splashRadius: 24,
-                      onPressed: _deleteFavorite,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
+              );
+            }
           );
         }
       ),
@@ -47,7 +57,4 @@ class _FavoriteState extends State<Favorite> {
     print('スポットへ移動！');
   }
 
-  void _deleteFavorite() {
-    print('このお気に入りを削除しますか？');
-  }
 }
