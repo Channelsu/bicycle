f0788d6c6f493e4a0cf51c79d234140338edf76a
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-05-30 01:43:39 +0900

    お気に入りスポットの削除メソッド追加

--- lib/provider/favorite_provider.dart
+++ lib/provider/favorite_provider.dart
@@ -1,3 +1,4 @@
+import 'package:bicycle/data/favorite_data.dart';
 import 'package:bicycle/model/favorite.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,18 @@ class FavoriteProvider extends ChangeNotifier {
     Favorite(id: 1, spotName: '府中駐輪場'),
     Favorite(id: 2, spotName: '狛江第二駐輪場'),
     Favorite(id: 3, spotName: '新宿中央駐輪場'),
+    Favorite(id: 4, spotName: '川崎第一駐輪場'),
+    Favorite(id: 5, spotName: '横浜公園自転車置き場'),
   ];
 
   List<Favorite> get favoriteList => _favoriteList;
+
+  void addFavoriteSpot() {
+    notifyListeners();
+  }
+
+  void deleteFavoriteSpot(int id) {
+    _favoriteList.removeWhere((favorite) => favorite.id == id);
+    notifyListeners();
+  }
 }
