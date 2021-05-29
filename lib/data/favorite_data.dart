f3cf7051a4fc39ad574e2c18eb4700d82d80ee18
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-05-30 00:47:20 +0900

    配列の型変更

--- lib/data/favorite_data.dart
+++ lib/data/favorite_data.dart
@@ -1,14 +1,7 @@
-List<Map<String, dynamic>> favorites = [
-  {
-    'id': 1,
-    'name': '府中駐輪場',
-  },
-  {
-    'id': 2,
-    'name': '狛江第二駐輪場',
-  },
-  {
-    'id': 3,
-    'name': '新宿中央駐輪場',
-  },
+import 'package:bicycle/model/favorite.dart';
+
+List<Favorite> favorites = [
+  Favorite(id: 1, spotName: '府中駐輪場'),
+  Favorite(id: 2, spotName: '狛江第二駐輪場'),
+  Favorite(id: 3, spotName: '新宿中央駐輪場'),
 ];
